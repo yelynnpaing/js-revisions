8e00755e4323adb9664b048bf8f20c522cfb3b01
J Theo <yelynnpaing.mm@gmail.com>
2024-06-11 23:40:46 +0630

    add simple e-cormmerce UI (landing page)

---
+++ vite-arac-ecormmerce/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+      },
+    },
+    fontFamily:{
+      sans:["Poppins", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
